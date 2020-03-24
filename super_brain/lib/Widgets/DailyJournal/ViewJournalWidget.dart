da7955099439532fe45d531b8ff8eb98b7b312d6
Feldrise <admin@feldrise.com>
2020-03-24 11:51:51 +0100

    [Update] Dialogs

--- super_brain/lib/Widgets/DailyJournal/ViewJournalWidget.dart
+++ super_brain/lib/Widgets/DailyJournal/ViewJournalWidget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:super_brain/Models/JournalItem.dart';
 import 'package:super_brain/Translations.dart';
 import 'package:super_brain/Widgets/InputDialog.dart';
@@ -25,55 +26,126 @@ class ViewJournalWidgetState extends State<ViewJournalWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            Text(journalItem.title, style: TextStyle(fontWeight: FontWeight.bold),),
-            SizedBox(height: 24),
-            Expanded(
-              child: Text(journalItem.content), 
-            ),
-            FlatButton(
-              onPressed: () async {
-                final String text = await InputDialog.asyncStringDialog(
-                    context,
-                    Translations.of(context).text("dialog_journal_title_description"),
-                    Translations.of(context).text("dialog_journal_title_title"),
-                    Translations.of(context).text("dialog_journal_title_exemple"),
-                    journalItem.content);
-                if (text.isNotEmpty) {
-                  setState(() {
-                    journalItem.content = text;
-                  });
+    return Stack(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(
+            top: 82, // 66 + 16,
+            bottom: 16,
+            left: 16,
+            right: 16,
+          ),
+          margin: EdgeInsets.only(top: 66),
+          decoration: new BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(8),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10.0,
+                offset: const Offset(0.0, 10.0),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // To make the card compact
+            children: <Widget>[
+              Text(
+                journalItem.title,
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Text(
+                    journalItem.content,
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 24.0),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    FlatButton(
+                      onPressed: () async {
+                        final String text = await InputDialog.asyncStringDialog(
+                            context,
+                            Translations.of(context).text("dialog_journal_title_description"),
+                            Translations.of(context).text("dialog_journal_title_title"),
+                            Translations.of(context).text("dialog_journal_title_exemple"),
+                            journalItem.content);
+                        if (text.isNotEmpty) {
+                          setState(() {
+                            journalItem.content = text;
+                          });
 
-                  widget.onJournalEdited(journalItem);
-                }
-              }, 
-              child: Text(Translations.of(context).text("edit_title"))
-            ),
-            FlatButton(
-              onPressed: () async {
-                final String text = await InputDialog.asyncStringDialog(
-                    context,
-                    Translations.of(context).text("dialog_journal_content_description"),
-                    Translations.of(context).text("dialog_journal_content_title"),
-                    Translations.of(context).text("dialog_journal_content_exemple"),
-                    journalItem.title);
-                if (text.isNotEmpty) {
-                  setState(() {
-                    journalItem.title = text;
-                  });
+                          widget.onJournalEdited(journalItem);
+                        }
+                      }, 
+                      child: Text(Translations.of(context).text("edit_content"), style: TextStyle(color: Theme.of(context).accentColor))
+                    ),
+                    FlatButton(
+                      onPressed: () async {
+                        final String text = await InputDialog.asyncStringDialog(
+                            context,
+                            Translations.of(context).text("dialog_journal_content_description"),
+                            Translations.of(context).text("dialog_journal_content_title"),
+                            Translations.of(context).text("dialog_journal_content_exemple"),
+                            journalItem.title);
+                        if (text.isNotEmpty) {
+                          setState(() {
+                            journalItem.title = text;
+                          });
 
-                  widget.onJournalEdited(journalItem);
-                }
-              }, 
-              child: Text(Translations.of(context).text("edit_content"))
-            ),
-          ],
+                          widget.onJournalEdited(journalItem);
+                        }
+                      }, 
+                      child: Text(Translations.of(context).text("edit_title"), style: TextStyle(color: Theme.of(context).accentColor))
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        Navigator.of(context).pop(); // To close the dialog
+                      },
+                      child: Text(Translations.of(context).text("ok"), style: TextStyle(color: Theme.of(context).accentColor),),
+                    ),
+                  ],
+                ),
+                // child: FlatButton(
+                //   onPressed: () {
+                //     Navigator.of(context).pop(); // To close the dialog
+                //   },
+                //   child: Text(Translations.of(context).text("ok"), style: TextStyle(color: Theme.of(context).accentColor),),
+                // ),
+              ),
+            ],
+          ),
         ),
-      ),
+        Positioned(
+          left: 16,
+          right: 16,
+          child: CircleAvatar(
+            backgroundColor: Theme.of(context).primaryColor,
+            radius: 66,
+            child: Padding(
+              padding: EdgeInsets.all(24),
+              child: SvgPicture.asset(
+                'assets/icon/icon.svg',
+                semanticsLabel: "Icon",
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- super_brain/lib/Widgets/DreamsJournal/ViewDreamWidget.dart
+++ super_brain/lib/Widgets/DreamsJournal/ViewDreamWidget.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:super_brain/Models/DreamItem.dart';
 import 'package:super_brain/Translations.dart';
 import 'package:super_brain/Widgets/InputDialog.dart';
@@ -26,36 +27,108 @@ class ViewDreamWidgetState extends State<ViewDreamWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            Text(dreamItem.date, style: TextStyle(fontWeight: FontWeight.bold),),
-            SizedBox(height: 24),
-            Expanded(
-              child: Text(dreamItem.text), 
-            ),
-            FlatButton(
-              onPressed: () async {
-                final String text = await InputDialog.asyncStringDialog(
-                    context,
-                    Translations.of(context).text("dialog_dream_description"),
-                    Translations.of(context).text("dialog_dream_title"),
-                    Translations.of(context).text("dialog_dream_exemple"),
-                    dreamItem.text);
-                if (text.isNotEmpty) {
-                  setState(() {
-                    dreamItem.text = text;
-                  });
+    return Stack(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(
+            top: 82, // 66 + 16,
+            bottom: 16,
+            left: 16,
+            right: 16,
+          ),
+          margin: EdgeInsets.only(top: 66),
+          decoration: new BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(8),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10.0,
+                offset: const Offset(0.0, 10.0),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // To make the card compact
+            children: <Widget>[
+              Text(
+                dreamItem.date,
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Text(
+                    dreamItem.text,
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 24.0),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    FlatButton(
+                      onPressed: () async {
+                        final String text = await InputDialog.asyncStringDialog(
+                            context,
+                            Translations.of(context).text("dialog_dream_description"),
+                            Translations.of(context).text("dialog_dream_title"),
+                            Translations.of(context).text("dialog_dream_exemple"),
+                            dreamItem.text);
+                        if (text.isNotEmpty) {
+                          setState(() {
+                            dreamItem.text = text;
+                          });
 
-                  widget.onDreamEdited(dreamItem);
-                }
-              }, 
-              child: Text(Translations.of(context).text("edit"))),
-          ],
+                          widget.onDreamEdited(dreamItem);
+                        }
+                      }, 
+                      child: Text(Translations.of(context).text("edit"), style: TextStyle(color: Theme.of(context).accentColor),)
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        Navigator.of(context).pop(); // To close the dialog
+                      },
+                      child: Text(Translations.of(context).text("ok"), style: TextStyle(color: Theme.of(context).accentColor),),
+                    ),
+                  ],
+                ),
+                // child: FlatButton(
+                //   onPressed: () {
+                //     Navigator.of(context).pop(); // To close the dialog
+                //   },
+                //   child: Text(Translations.of(context).text("ok"), style: TextStyle(color: Theme.of(context).accentColor),),
+                // ),
+              ),
+            ],
+          ),
         ),
-      ),
+        Positioned(
+          left: 16,
+          right: 16,
+          child: CircleAvatar(
+            backgroundColor: Theme.of(context).primaryColor,
+            radius: 66,
+            child: Padding(
+              padding: EdgeInsets.all(24),
+              child: SvgPicture.asset(
+                'assets/icon/icon.svg',
+                semanticsLabel: "Icon",
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
